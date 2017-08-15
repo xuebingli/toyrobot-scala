0644b53cab0cd8b441d4c5c35c0704d0d4186b6d
Xuebing Li <xuebinglee@gmail.com>
2017-08-15 12:49:54 +1000

    Add entry point and sample input

--- src/main/scala/Toyrobot.scala
+++ src/main/scala/Toyrobot.scala
@@ -1,3 +1,36 @@
+import scala.io.Source.fromFile
+import java.io.{FileNotFoundException, IOException}
+
 object Toyrobot extends App {
-  println("Hello world!")
+  def parsePlaceCommand(command: String): (Int, Int, Orientation) = {
+    val Array(_, info) = command.split(" ")
+    val Array(x, y, orientation) = info.split(",")
+    return (x.toInt, y.toInt, Orientation.fromString(orientation))
+  }
+
+  if (args.length == 1) {
+    val board = new Board()
+    val filename = args(0)
+    try {
+      val lines = fromFile(filename).getLines.toList
+      lines.foldLeft(None: Option[Robot]) { (robot, command) =>
+        command match {
+          case c if c == "REPORT" => robot.map(_.report)
+          case c if c == "MOVE"   => robot.map(_.move)
+          case c if c == "LEFT"   => robot.map(_.left)
+          case c if c == "RIGHT"  => robot.map(_.right)
+          case c if c.startsWith("PLACE") =>
+            val (x, y, orientation) = parsePlaceCommand(c)
+            val newRobot = Robot(x, y, orientation, board)
+            if (newRobot.nonEmpty) newRobot else robot
+        }
+      }
+    } catch {
+      case _: FileNotFoundException => println(s"File not found at $filename")
+      case e: IOException           => println(s"IOException $e")
+    }
+  } else {
+    println("Usage: Toyrobot instructions.txt")
+    println("Usage: sbt \"run instructions.txt\"")
+  }
 }

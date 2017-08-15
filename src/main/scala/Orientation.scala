1efcc5d7892765efd6d29df4e19b366266031dbc
Xuebing Li <xuebinglee@gmail.com>
2017-08-15 12:49:36 +1000

    Add fromString to Orientation

--- src/main/scala/Orientation.scala
+++ src/main/scala/Orientation.scala
@@ -3,3 +3,12 @@ case object NORTH extends Orientation
 case object EAST extends Orientation
 case object SOUTH extends Orientation
 case object WEST extends Orientation
+
+object Orientation {
+  def fromString(s: String): Orientation = s match {
+    case "NORTH" => NORTH
+    case "EAST"  => EAST
+    case "SOUTH" => SOUTH
+    case "WEST"  => WEST
+  }
+}
